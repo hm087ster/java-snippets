0e19330f18abbb25075373ac4e1470bcd326a4e4
hm087ster <51844013+hm087ster@users.noreply.github.com>
2021-05-23 11:29:19 +0100

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,15 @@
+
+public class Search {
+
+	public static void main(String[] args) {
+		
+		String str = "abcdefghijklmnopqrstuvwxxyz";
+		
+		if(str.indexOf("h")!= -1){
+			System.out.println("h is part of str");
+			
+		}
+
+	}
+
+}
