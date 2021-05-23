0e8ed900b5e2396981e7739ca267f1c35975db5d
hm087ster <51844013+hm087ster@users.noreply.github.com>
2021-05-23 10:45:52 +0100

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,29 @@
+
+public class Calc {
+
+	static int Add(int a, int b){
+		return (a+b);
+	}
+	
+	static int Subtract(int c, int d){
+		return (c-d);
+	}
+	
+	static int Multiply(int e, int f){
+		return (e*f);
+	}
+	
+	static int Divide(int g, int h){
+		return (g/h);
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Add(5,5));
+		System.out.println(Subtract(5,5));
+		System.out.println(Multiply(5,5));
+		System.out.println(Divide(5,5));
+		
+	}
+
+}
